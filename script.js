1ec8855ad0c035f90f057e66a06e9a4827d93aeb
Andrzej Gieszcz <agieszcz@vp.pl>
2019-09-13 01:15:48 +0200

    vue 12.09.2

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 new Vue({
     el: '#vue-app',
     data: {
-      name: 'Andrzej',
-      job: 'assassin',
-      website: 'http://www.thenetninja.co.uk',
-      websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>',
-      age: 29,
-      x: 0,
-      y: 0,
+    //   name: 'Andrzej',
+    //   job: 'assassin',
+    //   website: 'http://www.thenetninja.co.uk',
+    //   websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>',
+    //   age: 29,
+    //   x: 0,
+    //   y: 0,
     },
     methods: {
-        add: function(inc){
-            this.age += inc;
+        logName: function(){
+            console.log('enter name')
         },
-        subtact: function(dec){
-            this.age -= dec;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function(){
-            alert('Clicked me');
+        logAge: function(){
+            console.log('enter age')
         }
+        // add: function(inc){
+        //     this.age += inc;
+        // },
+        // subtact: function(dec){
+        //     this.age -= dec;
+        // },
+        // updateXY: function(event){
+        //     this.x = event.offsetX;
+        //     this.y = event.offsetY;
+        // },
+        // click: function(){
+        //     alert('Clicked me');
+        // }
     //   greet: function(time){
     //     return 'Good ' + time + ' ' + this.name;
     //   }
